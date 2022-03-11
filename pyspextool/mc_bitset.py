8e1dcd3110a015333e57165e64ba11e47fe87daa
kelle <kellecruz@gmail.com>
2022-03-11 13:27:57 -0500

    reformat of bitset function

--- pyspextool/mc_bitset.py
+++ pyspextool/mc_bitset.py
@@ -1,46 +1,50 @@
 import numpy as np
 
 
-def mc_bitset(array, bits):
+def bitset(array, bits):
     """
     To determine if the given bits are set in an array.
 
-    Input Parameters:
-        array - A numpy array to search.
-        bits  - A list or numpy array of bits to search.
-                Note that the "first" bit is denoted as zero,
-                while the "second" bit is denoted as 1.
+    Input Parameters
+    ----------------
+        array : array
+            A numpy array to search.
+        bits : list or array
+            A list or numpy array of bits to search.
+            Note that the "first" bit is denoted as zero,
+            while the "second" bit is denoted as 1.
 
     Optional Parameters:
         None
 
-    Output Parameters:
+    Returns
+    --------
+    array
         Returns a byte array of the same size as array.  A pixel is
         set if any of the bits requested are set in the same pixel
         of array.
 
-    Procedure:
+    Procedure
+    ---------
         Uses the Gumley IDL ishft technique.
 
-    Example:
+    Example
+    --------
         print(mc_bitset(np.array([3,4,1]),[0]))
         [1 0 1]
 
-    Modification History:
+    Modification History
+    --------------------
         2022-03-09 - Written by M. Cushing, University of Toledo.
                      Based on the mc_bitset.pro IDL program.
     """
     
-#  Define empty mask
+    #  Define empty mask
+    mask = np.zeros_like(array, dtype=np.int8)
 
-    mask = np.zeros_like(array,dtype=np.int8)
-
-#  Loop over every bit requested and identify those pixels for which that bit is set.
-#  Yes, that isn't very helpful when it comes to what is actually happening down there.
-    
+    #  Loop over every bit requested and identify those pixels for which that bit is set.
     for val in bits:
-
         tmp = (array >> val) & 1
         mask = mask | tmp    
 
-    return(mask)
+    return mask
