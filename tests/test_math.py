5331cbf7252aef0bfd4794aebd1251ff08d4eae0
Michael Cushing <michael.cushing@gmail.com>
2022-08-08 14:26:52 -0400

    tests

---
+++ tests/test_math.py
@@ -0,0 +1,11 @@
+from pyspextool.utils.math import *
+import numpy as np
+
+
+def test_round():
+
+    result = round(np.array([-3.5,-2.5,2.5,3.5]))
+    assert result[0] == -4.0
+    assert result[1] == -3.0
+    assert result[2] == 3.0
+    assert result[3] == 4.0    
