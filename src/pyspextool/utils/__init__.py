09552089c8e92ee6b1c5405d8e6698953b7b0bde
Michael Cushing <michael.cushing@gmail.com>
2022-08-12 14:08:44 -0400

    Added new modules.

--- src/pyspextool/utils/__init__.py
+++ src/pyspextool/utils/__init__.py
@@ -1,4 +1,5 @@
 """
 This subpackage contains functions for the utility framework.
 """
-__all__ = ['add_entry','arrays','coords','for_print','loop_progress','math','split_text']
+__all__ = ['add_entry', 'arrays', 'coords', 'for_print', 'loop_progress',
+           'math', 'split_text']
