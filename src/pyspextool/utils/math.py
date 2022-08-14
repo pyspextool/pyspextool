6a7788e132ad83cce06944d1670cd2ad976e6c52
Michael Cushing <michael.cushing@gmail.com>
2022-08-14 13:26:50 -0400

    fixed exponential format.

--- src/pyspextool/utils/math.py
+++ src/pyspextool/utils/math.py
@@ -537,7 +537,7 @@ def scale_data_stack(stack, *var, mask=None, idx=None):
 
     # Build the final scale array and do the math
 
-    sclar = np.tile(np.reshape(scales, reshape_info), tile_info)
+    sclarr = np.tile(np.reshape(scales, reshape_info), tile_info)
 
     sstack = stack * sclarr
 
