97bf6ba158d5df8356743ea969875f659d5e6395
Michael Cushing <michael.cushing@gmail.com>
2023-05-08 15:18:03 -0400

    Updated for new file structure.

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,16 +1,16 @@
-from pyspextool.extract import config
+from pyspextool import config as setup
 from pyspextool.setup_utils import *
 
 
 def test_pyspextool_setup():
-    set_paths(verbose=True)
+    set_parameters(verbose=True)
 
 
 def test_set_instrument_state():
     set_instrument('uspex')
 
-    assert config.state['instrument_name'] == 'uspex'
-    assert config.state['lincormax'] == 35000
+    assert setup.state['instrument'] == 'uspex'
+    assert setup.state['lincormax'] == 35000
 
     # TODO: add tests for not_supported instrument
 
