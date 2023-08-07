b2e4ef1c9a3742a6a301b3b4918486030412e286
kelle <kellecruz@gmail.com>
2023-08-07 17:29:26 -0400

    basic test of spex-prism test data

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,9 +1,23 @@
-from pyspextool import config as setup
 from pyspextool.setup_utils import *
+
+# This should be a dictionary
+
+base_folder = 'tests/test_data/spex-prism'
+# what instrument and mode we are using
+instrument = 'spex'
+mode = 'prism'
+
+# set output pathways
+# set output pathways
+raw_path = base_folder+'/data/'
+cal_path = base_folder+'/cals/'
+proc_path = base_folder+'/proc/'
+qa_path = base_folder+'/qa/'
 
 
 def test_pyspextool_setup():
-    set_parameters(verbose=True)
+    pyspextool_setup(instrument,raw_path=raw_path, cal_path=cal_path, proc_path=proc_path, qa_path=qa_path,verbose=True)
+
 
 
 def test_set_instrument_state():
