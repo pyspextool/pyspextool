7b7456413511d786c7bbf784d81036f54cac3b03
Kelle Cruz <kellecruz@gmail.com>
2023-08-11 14:20:21 -0400

    Adding batch script to the tests (#126)
    * add try/except for git lfs pointers to data files
    
    * batch test for  spex-prism
    
    * remove proc folder
    
    * Revert "add try/except for git lfs pointers to data files"
    
    This reverts commit c74aa8e39b6a8939bfaebb08b06828b73c896c51.

--- src/pyspextool/setup_utils.py
+++ src/pyspextool/setup_utils.py
@@ -11,6 +11,7 @@ from importlib.resources import files  # Python 3.10+
 
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.INFO)
 
+
 def pyspextool_setup(
     instrument=setup.state["instruments"][0],
     raw_path: str = None,
@@ -127,7 +128,7 @@ def pyspextool_setup(
     QA Write: {setup.state['qa_write']}
     QA path: {setup.state['qa_path']}
     QA Extension: {setup.state['qa_extension']}
-    
+
     Verbose: {setup.state['verbose']}
     """
 
@@ -223,10 +224,11 @@ def set_paths(paths: dict = None):
         try:
             paths["cal_path"] = check_path(paths["cal_path"], make_absolute=True)
             logging.debug(f"Set cal_path to {paths['cal_path']}")
-        except ValueError:
-            os.mkdir(paths["cal_path"])
-            paths["cal_path"] = check_path(paths["cal_path"], make_absolute=True)
-            logging.info(f"Created cal_path directory {paths['cal_path']}")
+        except ValueError as e:
+            # os.mkdir(paths["cal_path"])
+            # paths["cal_path"] = check_path(paths["cal_path"], make_absolute=True)
+            logging.error(f"Can't verify cal_path directory {paths['cal_path']}")
+            raise (e)
 
         setup.state["cal_path"] = paths["cal_path"]
     else:
@@ -419,10 +421,11 @@ def set_qa_state(
         try:
             qa_path = check_path(qa_path, make_absolute=True)
             logging.debug(f"Set qa_path to {qa_path}")
-        except ValueError:
-            os.mkdir(qa_path)
-            check_path(qa_path, make_absolute=True)
-            logging.info(f"Created qa_path directory {qa_path}")
+        except ValueError as e:
+            # os.mkdir(qa_path)
+            # check_path(qa_path, make_absolute=True)
+            logging.error(f"Can't verify qa_path directory {qa_path}")
+            raise (e)
 
         setup.state["qa_path"] = qa_path
     else:
@@ -452,11 +455,10 @@ def set_qa_state(
     QA Setup
     ----------------
     QA Show: {setup.state['qa_show']}
-    
+
     QA Write: {setup.state['qa_write']}
     QA Path: {setup.state['qa_path']}
     QA Extension: {setup.state['qa_extension']}
-    
     """
     logging.debug(msg)
 
