0c3cf40c7201c812b645eb817b138739fb6299de
kelle <kellecruz@gmail.com>
2024-12-24 15:38:14 -0500

    angle tests

--- tests/test_coords.py
+++ tests/test_coords.py
@@ -2,24 +2,26 @@ import numpy as np
 from pyspextool.utils.coords import ten, sixty
 import math 
 import pytest
-from astropy.coordinates import SkyCoord
+from astropy.coordinates import Angle
 import astropy.units as u
 
-@pytest.mark.parametrize('value,expected', 
+@pytest.mark.parametrize('input_ten, input_angle,expected', 
     [
-        ('-00:00:40.04424',-0.0111234), # ten('-00:00:40.04424')
-        ([-0.0,0.0,40.04424],-0.0111234), # ten([-0.0,0.0,40.04424])
-        ([0.0,0.0,-40.04424],-0.0111234), # ten([0.0,0.0,-40.04424])
-        (np.array([-0.0,0.0,40.04424]), -0.0111234), # ten(np.array([-0.0,0.0,40.04424]))
-        (np.array([0.0,0.0,-40.04424]), -0.0111234)  #ten(np.array([0.0,0.0,-40.04424]))
+        ('-00:00:40.04424', '-00:00:40.04424',-0.0111234), # ten('-00:00:40.04424')
+        ([-0.0,0.0,41.04424], '-0 0 41.04424',-0.011401178), # ten([-0.0,0.0,40.04424])
+        ([0.0,0.0,-42.04424], '-42.04424s', -0.011678955556), # ten([0.0,0.0,-40.04424])
+        (np.array([-0.0,0.0,43.04424]), '-0 0 43.04424', -0.0119567334), # ten(np.array([-0.0,0.0,40.04424]))
+        (np.array([0.0,0.0,-44.04424]), "-44.04424″", -0.012234511)  #ten(np.array([0.0,0.0,-40.04424]))
     ])
-def test_ten(value, expected):
-    result = ten(value)
-    assert math.isclose(result,expected)
+def test_ten(input_ten, input_angle,expected):
+    result = ten(input_ten)
+    assert np.isclose(result,expected)
+    print('ten', result)
 
-    c = SkyCoord(ra=1, dec=value, unit=('hourangle', 'deg'))
-    assert math.isclose(c.dec.value,expected), f"dec provided: {value}, dec returned: {c.dec}"
-    assert c.dec.unit.is_equivalent(u.degree)
+    angle = Angle(input_angle, unit=u.deg)
+    print('angle', angle)    
+    assert np.isclose(angle.value, expected), f"angle provided: {input_angle}, angle returned: {angle}"
+    assert angle.unit.is_equivalent(u.degree)
 
 def test_sixty():
 
