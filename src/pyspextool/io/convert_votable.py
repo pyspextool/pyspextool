675f766fb6ad3b86f0868149bd674319f536df65
kelle <kellecruz@gmail.com>
2025-07-23 14:14:08 -0400

    remove duplicate code

--- src/pyspextool/io/convert_votable.py
+++ src/pyspextool/io/convert_votable.py
@@ -10,86 +10,17 @@ from astropy.table import Table
 from astropy.io.votable import from_table, writeto
 from astropy.io.votable.tree import VOTableFile, Resource, Param
 from pyspextool.io.read_spectra_fits import read_spectra_fits
+from pyspextool.io import convert_fits
 
-__all__ = ["convert_to_votable", "spectrum_isplottable"]
+__all__ = ["convert_to_votable"]
 
 logger = logging.getLogger("pyspextool")
 
 
-def spectrum_isplottable(spectrum_path, raise_error=True, show_plot=False):
-    """
-    Check if spectrum is plottable
-    """
-    # load the spectrum and make sure it's a Spectrum1D object
-
-    try:
-        # spectrum: Spectrum1D = load_spectrum(spectrum_path) #astrodbkit2 method
-        spectrum = Spectrum1D.read(spectrum_path)
-    except Exception as e:
-        msg = (
-            str(e) + f"\n{spectrum_path}: \n" "unable to load file as Spectrum1D object"
-        )
-        if raise_error:
-            logger.error(msg)
-            raise IOError(msg)
-        else:
-            logger.warning(msg)
-            return False
-
-    # checking spectrum has good units and not only NaNs
-    try:
-        wave: np.ndarray = spectrum.spectral_axis.to(u.micron).value
-        flux: np.ndarray = spectrum.flux.value
-    except AttributeError as e:
-        msg = str(e) + f"{spectrum_path}: unable to parse spectral axis"
-        if raise_error:
-            logger.error(msg)
-            raise IOError(msg)
-        else:
-            logger.warning(msg)
-            return False
-    except u.UnitConversionError as e:
-        msg = e + f"{spectrum_path}: unable to convert spectral axis to microns"
-        if raise_error:
-            logger.error(msg)
-            raise IOError(msg)
-        else:
-            logger.warning(msg)
-            return False
-    except ValueError as e:
-        msg = e + f"{spectrum_path}: Value error"
-        if raise_error:
-            logger.error(msg)
-            raise IOError(msg)
-        else:
-            logger.warning(msg)
-            return False
-
-    # check for NaNs
-    nan_check: np.ndarray = ~np.isnan(flux) & ~np.isnan(wave)
-    wave = wave[nan_check]
-    flux = flux[nan_check]
-    if not len(wave):
-        msg = f"{spectrum_path}: spectrum is all NaNs"
-        if raise_error:
-            logger.error(msg)
-            raise IOError(msg)
-        else:
-            logger.warning(msg)
-            return False
-
-    if show_plot:
-        plt.plot(wave, flux)
-        plt.show()
-
-    return True
-
-
 def convert_to_votable(input_file, output_path="."):
     """
-    Convert a pyspextool file to a specutils compatibile FITS file
-    TODO: write tests
-    TODO: think about header keywords and Spectrum DM 1.2
+    Convert a pyspextool file to a VO Table
+    Note: this is a work in progress, not all features are implemented yet.
 
     Parameters
     ----------
@@ -100,46 +31,13 @@ def convert_to_votable(input_file, output_path="."):
     -------
     None
 
-    Writes converted FITS file to output_path
+    Writes converted VOTable file to output_path
 
     """
+    # Most of this is being developed in the convert_fits.py file
+    # If we want to convert to VOTable, we should use the make_table_of_spectra function to start
 
-    spectra, header_dict = read_spectra_fits(input_file)
-
-    wavelength = spectra[0, 0, :]
-    flux = spectra[0, 1, :]
-    flux_unc = spectra[0, 2, :]
-    # mask?
-
-    # header = compile_header(wavelength, **spectrum_info_all)
-    header = header_dict["header"]
-    header["HISTORY"] = (
-        f"Converted {basename(input_file)} using pyspextool.io convert_to_fits"
-    )
-
-    # replace spaces in object name with underscores
-    object_name = header["OBJECT"].replace(" ", "_").replace("/", "_")
-
-    # convert obsdate to format YYYYMonDD
-    obs_date = datetime.strptime(header["AVE_DATE"], "%Y-%m-%d").strftime("%Y%b%d")
-
-    # Update Date the file was written
-    header["DATE"] = date.today().strftime("%Y-%m-%d")
-
-    x_units = header["XUNITS"]
-    y_units = header["YUNITS"]
-    if y_units == "DN s-1":
-        y_units = "count s-1"
-
-    '''
-    spectrum_data_out = Table(
-        {
-            "wavelength": wavelength * u.Unit(x_units),
-            "flux": flux * u.Unit(y_units),
-            "flux_uncertainty": flux_unc * u.Unit(y_units),
-        }
-    )
-    '''
+    make_table_of_spectra = convert_fits.make_table_of_spectra(input_file)
 
     # Add in the spectrum data
     init_table['flux'].unit        = y_units
